37d20830ee2798a2d9d948eacb36747cdda93b1c
Sergei Gribanov <ssgribanov@gmail.com>
2020-10-23 14:44:23 +0700

    Lost particles in KsKPi hypo

---
+++ src/HypoKsPiMinusKPlusLost.cpp
@@ -0,0 +1,57 @@
+/*
+ * KFCmd library
+ * See LICENSE file at the top of the source tree.
+ *
+ * This product includes software developed by the
+ * CMD-3 collaboration (https://cmd.inp.nsk.su/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/**
+ * @file HypoKsPiMinusKPlusLost.cpp
+ *
+ * @brief Implementation of HypoKsPiMinusKPlusLost methods
+ *
+ * @ingroup KFCmd
+ *
+ * @author Sergei Gribanov
+ * Contact: ssgribanov@gmail.com
+ *
+ */
+
+#include "HypoKsPiMinusKPlusLost.hpp"
+
+#include <TDatabasePDG.h>
+
+#include "KPlusMeson.hpp"
+#include "PiMinusMeson.hpp"
+#include "PiPlusMeson.hpp"
+
+KFCmd::HypoKsPiMinusKPlusLost::HypoKsPiMinusKPlusLost(double energy, double magnetField,
+                                            long nIter, double tolerance)
+    : KFCmd::Hypothesis(energy, magnetField, nIter, tolerance) {
+  addVertex("vtx0");
+  addVertex("vtx1");
+  addChargedParticle(new KFCmd::PiPlusMeson("pi+_0"));
+  addChargedParticle(new KFCmd::PiMinusMeson("pi-_0"));
+  addChargedParticle(new KFCmd::PiMinusMeson("pi-_1"));
+  addParticlePxPyPzE("k+", TDatabasePDG::Instance()->GetParticle(321)->Mass() * 1000);
+  addVertexConstraintsXYZ("pi-_1", "vtx0");
+  addVertexConstraintsXYZ("pi+_0", "vtx1");
+  addVertexConstraintsXYZ("pi-_0", "vtx1");
+  addFlowConstraintsXYZ("ks-flow", "vtx0", "vtx1");
+  addParticleToFlow("ks-flow", "pi+_0");
+  addParticleToFlow("ks-flow", "pi-_0");
+}
+
+KFCmd::HypoKsPiMinusKPlusLost::~HypoKsPiMinusKPlusLost() {}
---
+++ src/HypoPiPlusKMinusKsWithPiPlusLost.cpp
@@ -0,0 +1,57 @@
+/*
+ * KFCmd library
+ * See LICENSE file at the top of the source tree.
+ *
+ * This product includes software developed by the
+ * CMD-3 collaboration (https://cmd.inp.nsk.su/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/**
+ * @file HypoPiPlusKMinusKsWithPiPlusLost.cpp
+ *
+ * @brief Implementation of HypoPiPlusKMinusKsWithPiPlusLost methods
+ *
+ * @ingroup KFCmd
+ *
+ * @author Sergei Gribanov
+ * Contact: ssgribanov@gmail.com
+ *
+ */
+
+#include "HypoPiPlusKMinusKsWithPiPlusLost.hpp"
+
+#include <TDatabasePDG.h>
+
+#include "KMinusMeson.hpp"
+#include "PiMinusMeson.hpp"
+#include "PiPlusMeson.hpp"
+
+KFCmd::HypoPiPlusKMinusKsWithPiPlusLost::HypoPiPlusKMinusKsWithPiPlusLost(double energy, double magnetField,
+                                              long nIter, double tolerance)
+    : KFCmd::Hypothesis(energy, magnetField, nIter, tolerance) {
+  addVertex("vtx0");
+  addVertex("vtx1");
+  addChargedParticle(new KFCmd::PiMinusMeson("pi-_0"));
+  addParticlePxPyPzE("pi+_0", TDatabasePDG::Instance()->GetParticle(211)->Mass() * 1000);  
+  addChargedParticle(new KFCmd::PiMinusMeson("pi+_1"));
+  addChargedParticle(new KFCmd::KMinusMeson("k-"));
+  addVertexConstraintsXYZ("pi+_1", "vtx0");
+  addVertexConstraintsXYZ("k-", "vtx0");
+  addVertexConstraintsXYZ("pi-_0", "vtx1");
+  addFlowConstraintsXYZ("ks-flow", "vtx0", "vtx1");
+  addParticleToFlow("ks-flow", "pi+_0");
+  addParticleToFlow("ks-flow", "pi-_0");
+}
+
+KFCmd::HypoPiPlusKMinusKsWithPiPlusLost::~HypoPiPlusKMinusKsWithPiPlusLost() {}
