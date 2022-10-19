4a7a103e2f54f5467edb155397cdbac894f8f199
Sergei Gribanov <ssgribanov@gmail.com>
2022-10-19 11:44:24 +0700

    adding new hypotheses

---
+++ hypos/include/kfcmd/hypos/Hypo2ChPionsAltPhoton.hpp
@@ -0,0 +1,61 @@
+/*
+ * KFCmd library
+ * See LICENSE file at the top of the source tree.
+ *
+ * This product includes software developed by the
+ * CMD-3 collaboration (https://cmd.inp.nsk.su/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/**
+ * @file Hypo2ChPionsAltPhoton.hpp
+ *
+ * @brief Hypo2ChPionsAltPhoton class definition
+ *
+ * @ingroup KFCmd
+ *
+ * @author Sergei Gribanov
+ * Contact: ssgribanov@gmail.com
+ *
+ */
+
+#ifndef _KFCmd_Hypo2ChPionsAltPhoton_HPP_
+#define _KFCmd_Hypo2ChPionsAltPhoton_HPP_
+#include "kfcmd/core/Hypothesis.hpp"
+
+namespace kfcmd {
+  namespace hypos {
+    /**
+     * Implementation of (pi+, pi-, gamma) hypothesis
+     * Alternative photon parametrization (without taking vertex into account)
+     */
+    class Hypo2ChPionsAltPhoton : public kfcmd::core::Hypothesis {
+    public:
+      //! A constructor
+      /*!
+       * @param energy (center-of-mass energy)
+       *
+       * @param magneticField (magnetic field)
+       *
+       * @param nIter (maximum number of iterations)
+       *
+       * @param tolerance (optimization tolerance)
+       */
+      Hypo2ChPionsAltPhoton(double, double, long = 20, double = 1.e-4);
+      //! A destructor
+      virtual ~Hypo2ChPionsAltPhoton();
+    };
+  } // namespace core
+}  // namespace kfcmd
+
+#endif
