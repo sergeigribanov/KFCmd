f96836d178a2543175e62028a6754dd11f24f6ba
Sergei Gribanov <ssgribanov@gmail.com>
2022-11-01 13:41:43 +0700

    fixed

--- core/src/Hypothesis.cpp
+++ core/src/Hypothesis.cpp
@@ -418,7 +418,7 @@ Eigen::MatrixXd kfcmd::core::Hypothesis::inverseMatrix(
 
 bool kfcmd::core::Hypothesis::fillTrack(const std::string& name, std::size_t index,
                                         const kfcmd::core::TrPh& data) {
-  Eigen::VectorXd par = Eigen::VectorXd::Zero(6); // 7
+  Eigen::VectorXd par = Eigen::VectorXd::Zero(7);
   const Float_t* fst_terr0 = &((data.terr0)[index][0][0]);
   const int s = 5;
   const int s_t = 5; // 6
