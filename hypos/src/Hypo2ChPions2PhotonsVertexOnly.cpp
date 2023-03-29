45676138e10e12cafd3d6f18612d9cea4309d06a
Sergei Gribanov <ssgribanov@gmail.com>
2023-03-29 10:55:05 +0700

    adding hypo

---
+++ hypos/src/Hypo2ChPions2PhotonsVertexOnly.cpp
@@ -0,0 +1,50 @@
+/*
+ * KFCmd library
+ * See LICENSE file at the top of the source tree.
+ *
+ * This product includes software developed by the
+ * CMD-3 collaboration (https://cmd.inp.nsk.su/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/**
+ * @file Hypo2ChPions2PhotonsVertexOnly.cpp
+ *
+ * @brief Implementation of Hypo2ChPions2PhotonsVertexOnly methods
+ *
+ * @ingroup KFCmd
+ *
+ * @author Sergei Gribanov
+ * Contact: ssgribanov@gmail.com
+ *
+ */
+
+#include "kfcmd/hypos/Hypo2ChPions2PhotonsVertexOnly.hpp"
+
+using namespace kfcmd::hypos;
+
+Hypo2ChPions2PhotonsVertexOnly::Hypo2ChPions2PhotonsVertexOnly(double energy,
+                                           double magneticField,
+                                           long nIter,
+                                           double tolerance)
+    : kfcmd::core::Hypothesis(energy, magneticField, nIter, tolerance) {
+  addVertexXYZ("vtx0");
+  auto pip = new kfcmd::core::PiPlusMeson("pi+");
+  addChargedParticle(pip);
+  auto pim = new kfcmd::core::PiMinusMeson("pi-");
+  addChargedParticle(pim);
+  addPhoton("g0", "vtx0");
+  addPhoton("g1", "vtx0");
+  addOutputVertexConstraintsXYZ("pi+", "vtx0");
+  addOutputVertexConstraintsXYZ("pi-", "vtx0");
+}
